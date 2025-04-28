0fbf42caa5a95ddfdd4aa721c6d298d026ad7c52
Assylbek Saduakhassov <as15453@nyu.edu>
2025-04-28 05:08:38 +0400

    Update backend api

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://167.172.42.145/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://api.husslify.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
